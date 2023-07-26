34f391ab76af2e880c9068c9d0ae8e1d658d5f4b
lucaslima18 <lucas.ala1999@gmail.com>
2023-07-26 04:11:51 -0300

    insert messages on CreateUserDTO

--- src/user/dto/CreateUser.dto.ts
+++ src/user/dto/CreateUser.dto.ts
@@ -1,16 +1,16 @@
 import { IsEmail, IsNotEmpty, IsString, IsStrongPassword, MinLength } from "class-validator";
 
 export class CreateUserDTO {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({message: "o nome de usuário deve ser do tipo string"})
+  @IsNotEmpty({message: "o nome de usuário não pode ser vazio"})
   name: string;
 
-  @IsEmail()
-  @IsNotEmpty()
+  @IsEmail({}, {message: 'O e-mail informado é inválido'})
+  @IsNotEmpty({message: "o email não pode ser vazio"})
   email: string;
 
-  @MinLength(6)
-  @IsStrongPassword()
-  @IsNotEmpty()
+  @MinLength(6, {message: "a senha deve conter no mínimo 6 caracteres"})
+  @IsStrongPassword(null, {message: "O password está fraco, tente inserir letras minúsculas, maiúsculas, números e caracteres especiais"})
+  @IsNotEmpty({message: "o password não pode ser vazio"})
   password: string;
 }
