599cd13b707f18f61f6aaa368773d0f326f201c8
lucaslima18 <lucas.ala1999@gmail.com>
2023-07-26 06:16:58 -0300

    add emailIsUnique validator decorator

--- src/user/dto/CreateUser.dto.ts
+++ src/user/dto/CreateUser.dto.ts
@@ -1,12 +1,14 @@
 import { IsEmail, IsNotEmpty, IsString, IsStrongPassword, MinLength } from "class-validator";
+import { emailIsUnique } from "../validator/EmailIsUnique.validator";
 
 export class CreateUserDTO {
   @IsString({message: "o nome de usuário deve ser do tipo string"})
   @IsNotEmpty({message: "o nome de usuário não pode ser vazio"})
   name: string;
 
-  @IsEmail({}, {message: 'O e-mail informado é inválido'})
+  @IsEmail(undefined, {message: 'O e-mail informado é inválido'})
   @IsNotEmpty({message: "o email não pode ser vazio"})
+  @emailIsUnique({message: "já existe um usuário cadastrado com este email"})
   email: string;
 
   @MinLength(6, {message: "a senha deve conter no mínimo 6 caracteres"})
