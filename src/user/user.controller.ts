fba73f0236d974e4315dcf792f357ff16ee03f1d
lucaslima18 <lucas.ala1999@gmail.com>
2023-07-26 06:15:34 -0300

    pass the UserRepository reference to the constructor for SOLID propouses | use userEntity to return correctly the createUser | make the listUsers return the ListUsersDTO

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,27 +1,36 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UserRepository } from "./user.repository";
 import { CreateUserDTO } from "./dto/CreateUser.dto";
+import { UserEntity } from "./user.entity";
+import { v4 as uuid } from 'uuid'
+import { ListUsersDTO } from "./dto/ListUsers.dto";
 
 @Controller('/users')
 export class UserController {
 
-  private userRepository = new UserRepository;
+  constructor(private userRepository: UserRepository) {}
 
   @Post()
   async createUser(@Body() userData: CreateUserDTO) {
-    this.userRepository.saveUser(userData);
-    return {
-      message: "user created!",
-      user_data: {
-        user_name: userData.name,
-        user_email: userData.email,
-        user_password: userData.password
-      }
-    };
+    const userEntity: UserEntity = new UserEntity();
+
+    userEntity.id = uuid();
+    userEntity.name = userData.name;
+    userEntity.email = userData.email;
+    userEntity.password = userData.password;
+
+    this.userRepository.saveUser(userEntity);
+    return { user: new ListUsersDTO(userEntity.id, userEntity.email), message: "user created!"};
   }
 
   @Get()
   async listUsers() {
-    return {all_users: await this.userRepository.listUsers()};
+    const all_users = await this.userRepository.listUsers()
+    return all_users.map(
+      user => new ListUsersDTO(
+        user.id,
+        user.name
+      )
+    )
   }
 }
